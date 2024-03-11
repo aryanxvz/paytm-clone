7cf9eee92976f500ffbd4f17b270e7fbe581d138
Aryan Mane <aryanmane7916@gmail.com>
2024-03-11 16:31:17 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+
+const JWT_SECRET = "aryansecret"
+
+module.exports = JWT_SECRET
---
+++ backend/db.js
@@ -0,0 +1,36 @@
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://aryanxvz:aryan0709@cluster0.qvmhxgy.mongodb.net/paytm")
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        maxLength: 15,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        maxLength: 15,
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = {
+    User
+}
