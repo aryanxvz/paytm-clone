47a193d378535ca888c68361df8998f2a80fcb98
Aryan Mane <aryanmane7916@gmail.com>
2024-03-14 21:38:16 +0530

    few changes

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,6 +1,9 @@
 
 const express = require("express");
 const { authMiddleware } = require("../middleware");
+const { Account } = require('../db');
+const { default: mongoose } = require('mongoose');
+
 const router = express.Router()
 
 router.get("/balance", authMiddleware, async (req, res) => {
